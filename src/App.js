c2c25447318282b77d9526e03605cf9b5602f7de
geekabdul <abdulrehmank762@gmail.com>
2022-09-05 13:17:06 +0530

    install react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+// import { Router } from 'workbox-routing';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        Welcome to My World
+      </Route>
+      <Route path="/second">
+        I failed lots of time but I will not loose hope, Before 2023 I will be
+        software developer
+      </Route>
+      <Route>this is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
