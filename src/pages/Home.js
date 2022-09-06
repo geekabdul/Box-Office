8028a3a02494f3e8c25f532c6994f37f4d065b8c
geekabdul <abdulrehmank762@gmail.com>
2022-09-06 12:40:34 +0530

    displayed showed results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
     // https://api.tvmaze.com/search/shows?q=boys
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>
-      r.json().then(result => {
-        console.log(result);
-      })
-    );
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>
+    //   r.json().then(result => {
+    //     setResults(result);
+    //     console.log(result);
+    //   })
+    // );
   };
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -20,6 +27,21 @@ const Home = () => {
       onSearch();
     }
     // console.log(ev.keyCode);
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -33,6 +55,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
