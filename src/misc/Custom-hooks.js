52267b740fd246853328c7914ac5220ac3c52b38
geekabdul <abdulrehmank762@gmail.com>
2022-09-29 10:47:57 +0530

    created two more hooks: useLastQuery and useshow

--- src/misc/Custom-hooks.js
+++ src/misc/Custom-hooks.js
@@ -1,4 +1,5 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useState } from 'react';
+import { apiGet } from './config';
 
 function showReducer(pervState, action) {
   switch (action.type) {
@@ -28,4 +29,65 @@ function usePersistedReducer(reducer, initialState, key) {
 
 export function useShows(key = 'shows') {
   return usePersistedReducer(showReducer, [], key);
+}
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : '';
+  });
+
+  const setPersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  };
+  return [input, setPersistedInput];
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  });
+  //   const [show, setShow] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(true);
+  //   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+          //   setShow(results);
+          //   setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+          //   setError(err.message);
+          //   setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, [showId]);
+
+  return state;
 }
